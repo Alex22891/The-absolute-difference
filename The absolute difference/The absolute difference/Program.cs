ce4ee51282fd6ac798542e7d90cc69bfa05f34f3
Alex22891 <103401307+Alex22891@users.noreply.github.com>
2022-11-25 16:48:10 +0900

    Add files via upload

---
+++ The absolute difference/The absolute difference/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace The_absolute_difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 48)
+                Console.WriteLine("Ответ: " + (n - 123) * 3);
+            else
+                Console.WriteLine("Ответ: " + Math.Abs(n - 123));
+        }
+    }
+}
